417fef721ed8699538f7d803a1d92f5bd51af31c
Anil Kumar Battula <anilkumarbattula1120@gmail.com>
2024-10-11 13:30:03 +0530

    code added

--- Sorting.java
+++ Sorting.java
@@ -67,12 +67,47 @@ public class Sorting {
             Quick_sort(arr, low, pivot-1);
             Quick_sort(arr, pivot+1, high);
         }
-        return arr;
+        return arr; 
+    }
+
+    public static void conquer(int arr[], int si, int mid, int ei) {
+        int merged[] = new int[ei-si+1];
+        int idx1 = si;
+        int idx2 = mid+1;
+        int x = 0;
+        while(idx1 <= mid && idx2 <= ei) {
+            if(arr[idx1] <= arr[idx2])
+            merged[x++] = arr[idx1++];
+            else
+            merged[x++] = arr[idx2++];
+        }
+
+        while(idx1 <= mid){
+            merged[x++] = arr[idx1++];
+        }
+
+        while(idx2 <= ei) {
+            merged[x++] = arr[idx2++];
+        }
+
+        for(int i=0, j=si; i < merged.length; i++,j++) {
+            arr[j] = merged[i];
+        }
+    }
+
+    public static void Merge_sort(int arr[], int si,int ei) {
+        if(si >= ei)
+            return ;
+        int mid = si + (ei-si)/2;
+        Merge_sort(arr,si,mid);
+        Merge_sort(arr,mid+1,ei);
+        conquer(arr,si,mid,ei);
     }
 
     public static void main (String args[]) {
         int arr[] = new int[]{3,7,1,8,4,2,2};
-        System.out.println(Arrays.toString(Quick_sort(arr, 0, arr.length-1)));
+        Merge_sort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
     
 }
